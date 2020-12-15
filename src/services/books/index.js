1bb6db3c934c67c0f9c89e1fe31a33f3320ef243
simonacossai <63307173+simonacossai@users.noreply.github.com>
2020-12-15 16:09:07 +0100

    DONE- crud for comments

--- src/services/books/index.js
+++ src/services/books/index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const uniqid = require("uniqid")
 const { getBooks, writeBooks } = require("../../fsUtilities")
 
 const booksRouter = express.Router()
@@ -56,7 +56,11 @@ booksRouter.post("/", async (req, res, next) => {
         error.message = "Book already in db"
         next(error)
       } else {
-        books.push(req.body)
+        newBook={
+            ...req.body,
+            comments:[],
+        }
+        books.push(newBook)
         await writeBooks(books)
         res.status(201).send({ asin: req.body.asin })
     }
@@ -116,4 +120,150 @@ booksRouter.delete("/:asin", async (req, res, next) => {
   }
 })
 
+booksRouter.get("/:asin/comments", async (req, res, next) => {
+    try {
+     const books = await getBooks()
+  
+      const bookFound = books.find(
+        book => book.asin === req.params.asin
+      )
+  
+      if (bookFound) {
+        res.send(bookFound.comments)
+      } else {
+        const error = new Error()
+        error.httpStatusCode = 404
+        next(error)
+      }
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+  })
+  
+  booksRouter.get("/:asin/comments/:commentID", async (req, res, next) => {
+    try {
+    const books = await getBooks()
+  
+      const bookFound = books.find(
+        book => book.asin === req.params.asin
+      )
+  
+      if (bookFound) {
+        const commentFound = bookFound.comments.find(
+          comment => comment.commentID === req.params.commentID
+        )
+        if (commentFound) {
+          res.send(commentFound)
+        } else {
+          const error = new Error()
+          error.httpStatusCode = 404
+          next(error)
+        }
+      } else {
+        const error = new Error()
+        error.httpStatusCode = 404
+        next(error)
+      }
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+  })
+  
+  booksRouter.post(
+    "/:asin/comments",
+    async (req, res, next) => {
+      try {
+        const books = await getBooks()
+  
+        const bookIndex = books.findIndex(
+          book => book.asin === req.params.asin
+        )
+        if (bookIndex !== -1) {
+          books[bookIndex].comments.push({
+            ...req.body,
+            commentID: uniqid(),
+            createdAt: new Date(),
+          })
+          await writeBooks(books)
+          res.status(201).send(books)
+        } else {
+          const error = new Error()
+          error.httpStatusCode = 404
+          next(error)
+        }
+      } catch (error) {
+        console.log(error)
+        next(error)
+      }
+    }
+  )
+  
+  booksRouter.put(
+    "/:asin/comments/:commentID",
+    async (req, res, next) => {
+      try {
+        const books = await getBooks()
+  
+        const bookIndex = books.findIndex(
+          book => book.asin === req.params.asin
+        )
+  
+        if (bookIndex !== -1) {
+          const commentIndex = books[bookIndex].comments.findIndex(
+            comment => comment._commentID === req.params.commentID
+          )
+  
+          if (commentIndex !== -1) {
+            const previousComment = books[bookIndex].comments[commentIndex]
+  
+            const updateComments = [
+              ...books[bookIndex].comments.slice(0, commentIndex), 
+              { ...previousComment, ...req.body, updatedAt: new Date() }, 
+              ...books[bookIndex].comments.slice(commentIndex + 1),
+            ] 
+            books[bookIndex].comments = updateComments
+  
+            await writeBooks(books)
+            res.send(books)
+          } else {
+            console.log("Comment not found")
+          }
+        } else {
+          console.log("BOOK not found")
+        }
+      } catch (error) {
+        console.log(error)
+        next(error)
+      }
+    }
+  )
+  
+  booksRouter.delete(
+    "/:asin/comments/:commentID",
+    async (req, res, next) => {
+      try {
+        const books = await getBooks()
+  
+        const bookIndex = books.findIndex(
+          book => book.asin === req.params.asin
+        )
+  
+        if (bookIndex !== -1) {
+          books[bookIndex].comments = books[bookIndex].comments.filter(
+            comment => comment.commentID !== req.params.commentID
+          )
+  
+          await writeBooks(books)
+          res.send(books)
+        } else {
+        }
+      } catch (error) {
+        console.log(error)
+        next(error)
+      }
+    }
+  )
+  
 module.exports = booksRouter
